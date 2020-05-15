0c73482eb51afb4ba9567ffce9be496bb5c254cf
justo23 <justojul23@hotmail.com>
2020-05-15 12:29:40 -0600

    - se agrego modulo de miembros
    (cherry picked from commit 352cbce0ef49eccaf624022f15aee482b66c1eb6)

---
+++ Miembros_W3/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2020 Miguel Fuentes Gomez
+#    (<http://www.addetec.com>).
+#
+##############################################################################
+
+{
+    'name': "Miembros W3BCentre",
+    'version': '13.0.1.0.0',
+    'author': 'Miguel Fuentes Addetec.com',
+    'maintainer': 'Addetec',
+    'website': 'http://www.addetec.com',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'summary': """ """,
+    'description': """ """,
+    'depends': ['sale'],
+    'data': [
+        'views/res_partner_view.xml',        
+    ],    
+    'installable': True,
+    'application': True,
+    'demo': [],
+    'test': []
+}
---
+++ Miembros_W3/models/res_partner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    como_contestar = fields.Text('Como Contestar')
+    notas_w3 = fields.Text('Indicaciones')
+    razon_social  = fields.Char('Razón Social', size=128, required=True)
+    ext_w3 = fields.Char('Ext W3')
+    codigo_w3 = fields.Char('Código W3')
