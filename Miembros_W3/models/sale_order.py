3443b8d8d95d9849eb5cfee2dffb983a14337e70
Justo <justo.rivera@xetechs.com>
2020-06-26 10:27:54 -0600

    version 4

--- Miembros_W3/models/sale_order.py
+++ Miembros_W3/models/sale_order.py
@@ -5,13 +5,6 @@ from odoo import models, fields, api
 class Partner(models.Model):
     _inherit = 'sale.order'
 
-    terminos_condiciones_w3 = fields.Selection(
-        [
-            ('oficina_virtual', 'Oficina Virtual'),
-            ('oficina_ejecutiva', 'Oficina Ejecutiva'),
-            ('sala_reuniones', 'Sala de Reuniones'),
-            ('sala_capacitaciones', 'Sala de Capacitaciones'),
-            ('focus_group', 'Focus Group'),
-            ('estandar', 'Estandar')
-        ]
-    )
+    terminos_condiciones_w3 = fields.Selection([('oficina_virtual', 'Oficina Virtual'),('oficina_ejecutiva', 'Oficina Ejecutiva'),
+    	('sala_reuniones', 'Sala de Reuniones'),('sala_capacitaciones', 'Sala de Capacitaciones'),('focus_group', 'Focus Group'),('estandar', 'Estandar')])
+    ocultar_total = fields.Boolean('Ocultar Total')
