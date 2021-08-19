e94ea4bc6b0be65d5066ad4901bf225983b5624a
Luis Aquino <luis.aquino@solucionesabacus.com>
2021-08-19 10:22:44 -0600

    sale subscription extends

---
+++ sale_subscription_extends/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sale Subscription FEL',
+    'version': '1.0.1',
+    'author': 'Xetechs, S.A.',
+    'website': 'https://www.xetechs.com',
+    'support': 'Luis Aquino --> laquino@xetechs.com',
+    'category': 'Accounting',
+    'depends': ['sale_subscription'],
+    'data': [],
+    'license': 'LGPL-3',
+    'application': True,
+    'installable': True,
+    'sequence': 2,
+    'auto_install': False,
+}
